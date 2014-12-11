4433f6ffba5a0915f34862642885d15190ea6947
unknown <Jipoom@.(none)>
2014-12-11 20:59:06 +0700

    hello

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/e-shopping/admin/');
+define('HTTP_CATALOG', 'http://localhost/e-shopping/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/e-shopping/admin/');
+define('HTTPS_CATALOG', 'http://localhost/e-shopping/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\e-shopping/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\e-shopping/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\e-shopping/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\e-shopping/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\e-shopping/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\e-shopping/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\e-shopping/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\e-shopping/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\e-shopping/system/upload/');
+define('DIR_LOGS', 'C:\xampp\htdocs\e-shopping/system/logs/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\e-shopping/system/modification/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\e-shopping/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'e_shopping');
+define('DB_PREFIX', '');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/e-shopping/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/e-shopping/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\e-shopping/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\e-shopping/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\e-shopping/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\e-shopping/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\e-shopping/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\e-shopping/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\e-shopping/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\e-shopping/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\e-shopping/system/upload/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\e-shopping/system/modification/');
+define('DIR_LOGS', 'C:\xampp\htdocs\e-shopping/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'e_shopping');
+define('DB_PREFIX', '');
